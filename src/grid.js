02af7235d2fa0b603b048ff06535962f016ba9e7
mrElnekave <klavlavigugu@gmail.com>
2021-02-12 23:34:31 -0800

    added neighbours

--- src/grid.js
+++ src/grid.js
@@ -4,17 +4,17 @@ class Grid{
     // tileSize is the diameter of the tile (edge to edge)
     constructor(height, width, x, y, tileSize){
         this.tiles = []; // Stores all tiles
-        this.initTilesArray(sideLength);
         this.pos = {x: x, y: y};
         this.tileSize = tileSize;
         this.dims = {h: height, w: width};
+        this.initTilesArray();
     }
 
-    initTilesArray(sideLength){
+    initTilesArray(){
         for (var i=0; i<this.dims.h; i++){
             var layer = [];
             for (var j=0; j<this.dims.w; j++){
-                layer.push(null);
+                layer.push(new BaseTile());
             }
             this.tiles.push(layer);
         }
@@ -24,7 +24,21 @@ class Grid{
 
     }
 
-    getNeighbors(tileCoord){
-
+    neighbours(row, col){
+        var locations = [[-1,1], [0,1], [1,1], [-1,0], [1,0], [-1,-1], [0,-1], [1,-1]];
+        var neighbours = [];
+        for (var i = 0; i<locations.length;i++){
+            var loc = locations[i];
+            var add_row = loc[0];
+            var add_col = loc[1];
+            var new_row = row + add_row;
+            var new_col = col + add_col;
+            if (0 <= new_row < this.tiles[0].length && 0 <= new_col < this.tiles.length){
+                var temp = [new_row, new_col];
+                neighbours.push(temp);
+            }
+          }
+        return neighbours
     }
+
 }
