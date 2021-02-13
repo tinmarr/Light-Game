8dc3c9ca5d7e1f02f9ae77509130ed71b9783d43
Martin Chaperot-Merino <martin.chapino+github@gmail.com>
2021-02-12 22:49:32 -0800

    Fix dumb shit

--- src/grid.js
+++ src/grid.js
@@ -4,13 +4,13 @@ class Grid{
     // tileSize is the diameter of the tile (edge to edge)
     constructor(height, width, x, y, tileSize){
         this.tiles = []; // Stores all tiles
-        this.initTilesArray(sideLength);
         this.pos = {x: x, y: y};
         this.tileSize = tileSize;
         this.dims = {h: height, w: width};
+        this.initTilesArray();
     }
 
-    initTilesArray(sideLength){
+    initTilesArray(){
         for (var i=0; i<this.dims.h; i++){
             var layer = [];
             for (var j=0; j<this.dims.w; j++){
