e77f50496a48d51d3001b762bebf8928dd728f15
mrElnekave <klavlavigugu@gmail.com>
2021-02-13 17:52:30 -0800

    fixed reflector, and writeText func

--- src/tiles/reflectorTile.js
+++ src/tiles/reflectorTile.js
@@ -7,16 +7,17 @@ class ReflectorTile extends EmptyTile{
     }
 
     changeLight(light){
-        var dirs = ['E', 'S', 'W', 'N'];
+      var dirs = ['E', 'S', 'W', 'N'];
         if (this.orientation == 0) {
-            var posChange = [[1, 1, 'S'], [-1, -1, 'W'], [-1, -1, 'N'], [1, 1, 'N']];
+            var posChange = [[1, 1, 'S'], [1, 1, 'E'], [-1, -1, 'N'], [-1, -1, 'W']];
         } else {
-            var posChange = [[1, -1, 'N'], [1, -1, 'E'], [-1, 1, 'S'], [-1, -1, 'W']];
+            var posChange = [[1, -1, 'N'], [-1, 1, 'W'], [-1, 1, 'S'], [1, -1, 'E']];
         }
         var newPos = {
             x: light.pos.x + posChange[dirs.indexOf(light.dir)][0],
             y: light.pos.y + posChange[dirs.indexOf(light.dir)][1],
         };
         this.grid.setTile(new Light(newPos.x, newPos.y, posChange[dirs.indexOf(light.dir)][2], light.color, this.grid));
+
     }
 }
