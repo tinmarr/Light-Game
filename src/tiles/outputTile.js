36dc105d5b81dc55b48487adafce3368980fc1c8
Martin Chaperot-Merino <martin.chapino+github@gmail.com>
2021-02-13 21:06:06 -0800

    Fix this.

--- src/tiles/outputTile.js
+++ src/tiles/outputTile.js
@@ -13,20 +13,19 @@ class OutputTile extends EmptyTile {
     reset() {
         this.sprite.setTexture(this.lightAccept + '-out-no');
     }
-    correctOrientation(lightDir){
-        if(this.orientation == 0) return lightDir == 'E';
-        if(this.orientation == 1) return lightDir == 'S';
-        if(this.orientation == 2) return lightDir == 'W';
-        if(this.orientation == 3) return lightDir == 'N';
+    correctOrientation(lightDir) {
+        if (this.orientation == 0) return lightDir == 'E';
+        if (this.orientation == 1) return lightDir == 'S';
+        if (this.orientation == 2) return lightDir == 'W';
+        if (this.orientation == 3) return lightDir == 'N';
     }
     changeLight(light) {
         // check if white light enters: this should be changed if we want different lights to enter the splitter
-        if (light.color == this.lightAccept && correctOrientation(light.dir)) {
+        if (light.color == this.lightAccept && this.correctOrientation(light.dir)) {
             this.sprite.setTexture(this.lightAccept + '-out-clear');
             return;
         } else {
             this.sprite.setTexture(this.lightAccept + '-out-wrong');
-
         }
     }
 }
