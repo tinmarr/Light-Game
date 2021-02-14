36c09f60208cf23315aa2655178e2f92a4572be6
mrElnekave <klavlavigugu@gmail.com>
2021-02-13 17:05:05 -0800

    added menu system

--- src/main.js
+++ src/main.js
@@ -22,6 +22,7 @@ var game = new Phaser.Game(config),
     updating = false,
     start = {x: 0, y: 0},
     tileSize = 34,
+    tintColor = 0x696a6a,
     starterLight;
 
 function preload(){
@@ -64,45 +65,13 @@ function preload(){
         makeURL('tiles', 'colorFilterTile'),
         makeURL('tiles', 'outputTile'),
     ]);
+
+    // music and sound effects
+    this.load.audio("main-menu_music", "assets/main-menu_music.mp3")
 }
 
 function create(){
-    // this makes something setDraggable
-    /*
-    var image = scene.add.sprite(200, 300, 'white-light').setInteractive();
-    image.on('pointerover', function () {
-
-        image.setTint(0x00ff00);
-
-    });
-
-    image.on('pointerout', function () {
-
-        image.clearTint();
-
-    });
-
-    scene.input.setDraggable(image);
-
-    scene.input.on('dragstart', function (pointer, gameObject) {
-
-        gameObject.setTint(0xff0000);
-
-    });
-
-    scene.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-        var pixelCoord = {x: dragX, y:dragY};
-        var newcoords = grid.getPixelCoords(grid.getGridCoords(pixelCoord));
-        gameObject.x = newcoords.x;
-        gameObject.y = newcoords.y;
-
-    });
-
-    scene.input.on('dragend', function (pointer, gameObject) {
-
-        gameObject.clearTint();
-
-    });*/
+    // menu();
     makeLevel(1);
 }
 
@@ -157,6 +126,72 @@ function keyBinds(e){
         }
     }
 }
+
+
+function levelSelect(){
+    scene.children.getChildren().splice(0, scene.children.getChildren().length); // clear canvas
+    // draw background
+    background = scene.add.image(0,0, "empty-tile").setOrigin(0).setScale(5).setDepth(0); // levelSelect background
+    //draw buttons and textboxes
+    let backButton = scene.add.image(background.displayWidth / 2, background.displayHeight / 2, "back_button").setDepth(1);
+
+    for (let i = 0; i < 5; i++){
+      var tempboi = scene.add.image(background.displayWidth / 2, background.displayHeight / 2, "level_" + i).setDepth(1);
+      tempboi.setInteractive();
+      tempboi.on("pointerover", ()=>{
+          tempboi.setTint(tintColor);
+          // console.log("in");
+      });
+      tempboi.on("pointerout", ()=>{
+          tempboi.clearTint();
+          // console.log("out");
+      });
+      tempboi.on("pointerup", ()=>{
+          makeLevel(i);
+          // level select
+      });
+    }
+    // interactivity
+    backButton.setInteractive();
+    backButton.on("pointerover", ()=>{
+        backButton.setTint(tintColor);
+        console.log("in");
+    });
+    backButton.on("pointerout", ()=>{
+        backButton.clearTint();
+        console.log("out");
+    });
+    backButton.on("pointerup", ()=>{
+        console.log("out");
+        menu();
+    });
+}
+
+
+function menu(){
+    scene.children.getChildren().splice(0, scene.children.getChildren().length); // clear canvas
+    scene.sound.pauseOnBlur = false;
+    // scene.sound.play("main-menu_music", {  --- uncomment when music is available
+    //   loop: true,
+    // });
+    background = scene.add.image(0,0, "empty-tile").setOrigin(0).setScale(5).setDepth(0); //main-menu_background get rid of setscale
+    let playButton = scene.add.image(background.displayWidth / 2, background.displayHeight / 2, "play_button").setDepth(1);
+
+    playButton.setInteractive();
+    playButton.on("pointerover", ()=>{
+        playButton.setTint(tintColor);
+        console.log("in");
+    });
+    playButton.on("pointerout", ()=>{
+        playButton.clearTint();
+        console.log("out");
+    });
+    playButton.on("pointerup", ()=>{
+        console.log("out");
+        levelSelect();
+    });
+}
+
 
 function makeLevel(levelNumber){
     scene.children.getChildren().splice(0, scene.children.getChildren().length); // clear canvas
