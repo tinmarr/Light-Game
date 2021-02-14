c97bd3031a7bdfe3317548b92316054b58efbaa8
Martin Chaperot-Merino <martin.chapino+github@gmail.com>
2021-02-14 01:16:25 -0800

    Merger Kinda and Level 1

--- src/tiles/mergerTile.js
+++ src/tiles/mergerTile.js
@@ -1,5 +1,11 @@
 class MergerTile extends EmptyTile {
-    constructor(x, y, grid, tileSize, pixelCoords = null) {
+    constructor(x, y, grid, tileSize, orientation, pixelCoords = null) {
         super(x, y, grid, tileSize, pixelCoords);
+        this.sprite.setTexture('merger-tile');
+        this.orientation = orientation; // the orientation refers to the white light position
+        // 0: left  1: top  2: right  3: bottom
+        if (this.orientation == 1) this.sprite.angle = 90;
+        if (this.orientation == 3) this.sprite.angle = -90;
+        if (this.orientation == 2) this.sprite.setFlipX(true);
     }
 }
